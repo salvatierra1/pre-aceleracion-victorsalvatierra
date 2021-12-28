17937895f4d1fc047cb51b9e9101f2be78380486
salvatierra1 <victordanielsalvatierra6@gmail.com>
2021-12-28 16:21:44 -0300

    Master - commit inicial

---
+++ src/main/java/com/disney/disneyworld/DisneyworldApplication.java
@@ -0,0 +1,13 @@
+package com.disney.disneyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyworldApplication.class, args);
+	}
+
+}
